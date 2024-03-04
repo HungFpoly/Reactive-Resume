f29bc521aa1e48586acc5a011f47d375091cb8bd
vacsdev_hungpcq <hungpcq@vacsvn.com>
2024-03-04 17:05:35 +0700

    update template

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -3,11 +3,22 @@ import * as ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
 import { router } from "./router";
+import { Helmet } from "react-helmet-async";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <StrictMode>
     <RouterProvider router={router} />
+    <Helmet>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <script>
+              { 
+                // @ts-ignore 
+                (adsbygoogle = window.adsbygoogle || []).push({})
+            }
+            </script>
+            {/* paste script code in between Tilde  */}
+          </Helmet>
   </StrictMode>,
 );
