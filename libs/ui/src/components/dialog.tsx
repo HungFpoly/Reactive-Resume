78fce97bfe11f5dafeb09a17f568a864463b5512
vacsdev_hungpcq <hungpcq@vacsvn.com>
2024-03-07 14:43:48 +0700

    fix

--- libs/ui/src/components/dialog.tsx
+++ libs/ui/src/components/dialog.tsx
@@ -62,7 +62,7 @@ DialogHeader.displayName = "DialogHeader";
 
 export const DialogFooter = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
-    className={cn("flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-3", className)}
+    className={cn("flex flex-col-reverse sm:flex-row sm:justify-center sm:space-x-3", className)}
     {...props}
   />
 );
